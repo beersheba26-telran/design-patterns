8a03d92e3d58c17559d5027bf464201f2814982b
Yuragranovsky <yuragranovsky@gmail.com>
2026-04-29 13:02:55 +0300

    iniversal function to log both positional and keyword arguments

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ from loguru import logger
 from logging_config import config_logger
 from typing import Any
 config_logger()
-def key_fun(**empl):
+def universal_fun(*args, **keys):
     '''
     empl parameter disgnates dict
     at function call, key argiments shoyld be passed
     '''
-    logger.debug(empl)
+    logger.debug(keys) # logging dict with key arguments
+    logger.debug(args) # logging tuple with positional arguments
+    logger.info("".join("_" for _ in range(50)))
     
-key_fun() 
-key_fun(id=1, name="Vasya")   
+universal_fun() 
+universal_fun(id=1, name="Vasya")   
+universal_fun(1)
+universal_fun(1, id=1, name="Vasya")
