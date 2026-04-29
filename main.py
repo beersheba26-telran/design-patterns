0810ae6a530ae5a1a5e8f28cf2bee181366e2315
Yuragranovsky <yuragranovsky@gmail.com>
2026-04-29 12:37:31 +0300

    variadic parameter implementation and usage example

--- main.py
+++ main.py
@@ -1,34 +1,13 @@
 from loguru import logger
 from logging_config import config_logger
-from abc import ABC, abstractmethod
-'''
-migartion from legacy interface to a new one
-'''
+from typing import Any
+config_logger()
+def variadic_fun(*args:Any):
+    '''
+    any number of arguments at a function call
+    args - paramter dersignating tuple with arguments
+    '''
+    logger.info(args)
 
-
-#####################################
-# leagacy interface and implementation
-class LegacyLogger(ABC):
-    @abstractmethod
-    def print_message(self, message: str):
-        pass
-
-class RegularPrint(LegacyLogger): 
-    def print_message(self, message: str) :
-        logger.debug(f"method print from legacy logger is called with message {message}")
-        print(message) 
-#########################################
-# new interface and adapter pattern implementation
-class NewLogger(ABC):
-    @abstractmethod
-    def info(self, message: str) :
-        pass 
-class AdapterLogger(NewLogger) :
-    def __init__(self,legacyLogger: LegacyLogger) :
-        self.__legacyLogger = legacyLogger 
-    def info(self, message: str) :
-        self.__legacyLogger.print_message(message) 
-###################################################
-adapterLogger = AdapterLogger(RegularPrint())
-adapterLogger.info("kuku")
-               
+variadic_fun()  # no arguments - empty tuple
+variadic_fun("abc", 10, [1, 2,3], ("a", 1))  
