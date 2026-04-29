665ba7e8415aa7213cc5650f593d43383f39524b
Yuragranovsky <yuragranovsky@gmail.com>
2026-04-29 11:22:46 +0300

    prototype pattern for user creation

--- main.py
+++ main.py
@@ -1,59 +1,16 @@
+import copy
 from loguru import logger
 from logging_config import config_logger
-from abc import ABC, abstractmethod
-from os import getenv
 config_logger()
-# abstract class Payment
-class Payment(ABC):
-    @abstractmethod
-    def pay(self,amount: int):
-        pass
-    @staticmethod
-    @abstractmethod
-    def create():
-        return _createPayment( getenv("PAYMENT_TYPE") )
-    ###############################
-    # Classes implemting method pay of abstract Payment
-class PayPalPayment(Payment):
-    def pay(self,amount: int):
-        logger.info(f"paypal payment of amount {amount} USD")
-    @staticmethod
-    def create()->Payment:
-        '''
-        possibly complexed functionality of creation PayPalPayment instance
-        '''
-        return PayPalPayment()
-            
-class CardPayment(Payment):
-    def pay(self, amount: int):
-        logger.info(f"credit card payment of amount {amount} USD") 
-    @staticmethod
-    def create()->Payment:
-        '''
-        possibly complexed functionality of creation CardPayment instance
-        '''
-        return CardPayment()    
-    #######################################
-    # Identification of different Payment classes - FACTORY
-FACTORY: dict[str, Payment] = {
-        "card": CardPayment.create,
-        "paypal": PayPalPayment.create
-}   
-# Factory method
-def _createPayment(paymentName: str) -> Payment:
-    res: Payment|None = None
-    try:
-        res: Payment = FACTORY[paymentName]()
-        logger.debug(f"{paymentName} class instance is returned")
-    except KeyError:
-        logger.error(f"{paymentName} not implemented as Payment class")
-    return res 
-################################################################
-# creating Payment 
+protoUser = {"role":"USER", "features":["read"]}
+protoAdmin = {"role":"ADMIN", "features": ["read","write"]}
+def createUser(*, role:str, name: str)->dict:
+    proto = protoUser if role == "USER" else protoAdmin
+    res = copy.deepcopy(proto)
+    res["name"] = name
+    return res
 
-payment: Payment = Payment.create() 
-############################################################
-# using in many code lines
-payment.pay(500)
-       
-                   
+user1 = createUser(name="Vasya", role="USER")
+user2 = createUser(name="Olya", role="ADMIN")
+logger.info(f"user1 is {user1}")
+logger.info(f"user2 is {user2}")
