867acd4a38195dc79b00d2a368fbc05efe6867ed
Yuragranovsky <yuragranovsky@gmail.com>
2026-05-01 15:44:22 +0300

    Observer , Observable (Subject) clsses and main with sanity testing

--- main.py
+++ main.py
@@ -1,59 +1,11 @@
-from loguru import logger
-from logging_config import config_logger
-from abc import ABC, abstractmethod
-from os import getenv
-config_logger()
-# abstract class Payment
-class Payment(ABC):
-    @abstractmethod
-    def pay(self,amount: int):
-        pass
-    @staticmethod
-    @abstractmethod
-    def create():
-        return _createPayment( getenv("PAYMENT_TYPE") )
-    ###############################
-    # Classes implemting method pay of abstract Payment
-class PayPalPayment(Payment):
-    def pay(self,amount: int):
-        logger.info(f"paypal payment of amount {amount} USD")
-    @staticmethod
-    def create()->Payment:
-        '''
-        possibly complexed functionality of creation PayPalPayment instance
-        '''
-        return PayPalPayment()
-            
-class CardPayment(Payment):
-    def pay(self, amount: int):
-        logger.info(f"credit card payment of amount {amount} USD") 
-    @staticmethod
-    def create()->Payment:
-        '''
-        possibly complexed functionality of creation CardPayment instance
-        '''
-        return CardPayment()    
-    #######################################
-    # Identification of different Payment classes - FACTORY
-FACTORY: dict[str, Payment] = {
-        "card": CardPayment.create,
-        "paypal": PayPalPayment.create
-}   
-# Factory method
-def _createPayment(paymentName: str) -> Payment:
-    res: Payment|None = None
-    try:
-        res: Payment = FACTORY[paymentName]()
-        logger.debug(f"{paymentName} class instance is returned")
-    except KeyError:
-        logger.error(f"{paymentName} not implemented as Payment class")
-    return res 
-################################################################
-# creating Payment 
-
-payment: Payment = Payment.create() 
-############################################################
-# using in many code lines
-payment.pay(500)
-       
-                   
+from observer_pattern import NewsAgency, EmailNotification, NewsPublisher
+newsCNN = NewsAgency("CNN")
+newsBBC = NewsAgency("BBC")
+emailNotification = EmailNotification("yuri@gmail.com", "david@tel-ran.co.il")
+newsPublisher = NewsPublisher()
+newsPublisher.subscribe(newsCNN)
+newsPublisher.subscribe(newsBBC)
+newsPublisher.subscribe(emailNotification)
+newsPublisher.notify("Breaking news")
+del newsBBC
+newsPublisher.notify("Good news")
---
+++ observer_pattern.py
@@ -0,0 +1,41 @@
+from abc import ABC, abstractmethod
+import weakref
+class Subject(ABC):
+    @abstractmethod
+    def subscribe(self,observer: "Observer"): pass
+    
+    @abstractmethod
+    def notify(self,message: str): pass
+    @abstractmethod
+    def message(self)-> str: pass
+class Observer(ABC):
+    @abstractmethod
+    def update(self,subject: Subject):
+        pass
+class NewsAgency(Observer) :
+    def __init__(self, name:str):
+        self.__name = name
+    def update(self, subject: Subject):
+        print(f"received {subject.message()} for {self.__name} agency")
+         
+class EmailNotification(Observer):
+    def __init__(self, *emails:str):
+        self.__emails = list(emails) 
+    def update(self, subject: Subject):
+        print(f" {subject.message()} has been sent to {self.__emails}")  
+class NewsPublisher(Subject):
+    __observers: list[weakref.ReferenceType[Observer]]
+    def __init__(self):
+        self.__observers = [] #list of the weak references to observers
+        self.__message = ""
+    def subscribe(self,observer: Observer):
+        self.__observers.append(weakref.ref(observer, lambda obs: self.__observers.remove(obs))) 
+    def notify(self, message: str):
+        self.__message = message 
+        for observer_ref in self.__observers:
+            observer_ref().update(self) 
+    def message(self):
+        return self.__message
+                
+
+
